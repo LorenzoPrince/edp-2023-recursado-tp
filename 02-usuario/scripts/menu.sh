ce75d20be91a625441a4585fc6642f58269251b4
Alumno <brunopace2001@gmail.com>
2023-11-17 20:06:15 -0300

    menu inicial

---
+++ 02-usuario/scripts/menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PS3="Elija> "
+echo "Hola, soy el menu del TP. ¿Que quiere hacer?"
+select opcion in "Clasificar archivos" "Monitorear Sistema" "Analizar texto" "Salir"
+do
+	[ -z "$opcion" ] && echo "elegir opcion valida! " && continue
+	[ $REPLY == 1 ] && bash clasificar.sh && continue
+	[ $REPLY == 2 ] && echo "elegio monitorear el sistema! " && continue
+	[ $REPLY == 3 ] && echo "elegio analizar texto! " && continue
+	[ $REPLY == 4 ] && echo "elegio salir! " && break
+
+	echo "Opcion elegida: " $opcion
+done 
+
+exit 0
