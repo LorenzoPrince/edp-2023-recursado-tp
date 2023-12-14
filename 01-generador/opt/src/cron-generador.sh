2f308560ea32479b9d2f757244af7904c4e02f93
franco.caballero.05@gmail.com <franco.caballero.05@gmail.com>
2023-12-14 18:00:59 +0000

    organizamos la estructura del generador
    Signed-off-by: franco.caballero.05@gmail.com <franco.caballero.05@gmail.com>

---
+++ 01-generador/opt/src/cron-generador.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+formats=("image" "sound" "text")
+
+if [[ $# -lt 1 ]]; then
+	echo "Argumentos invalidos"
+	exit 1
+fi
+
+number="$1"
+
+for i in $(seq $number); do
+	format=${formats[$RANDOM % ${#formats[@]}]}
+	$(dirname $0)/generador.sh $format
+done
